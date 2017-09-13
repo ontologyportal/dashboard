414ace1a3183fa3fab016c0200fe1f40ff84c712
mohit.gupta <mohit.gupta@BLRKEC156279L.ad.infosys.com>
2017-09-13 13:07:34 +0530

    Updated license header

--- src/com/articulate/dashboard/chartentities/ChartData.java
+++ src/com/articulate/dashboard/chartentities/ChartData.java
@@ -2,7 +2,9 @@ package com.articulate.dashboard.chartentities;
 
 /**
  * This code is copyright Infosys Ltd 2017.
- *
+ * This software is released under the GNU Public License.You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License.
+ * 
  * @author mohit.gupta
  */
 public class ChartData {
--- src/com/articulate/dashboard/chartentities/ScaleLabel.java
+++ src/com/articulate/dashboard/chartentities/ScaleLabel.java
@@ -2,10 +2,11 @@ package com.articulate.dashboard.chartentities;
 
 /**
  * This code is copyright Infosys Ltd 2017.
- *
+ * This software is released under the GNU Public License.You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License.
+ * 
  * @author mohit.gupta
  */
-
 public class ScaleLabel {
     boolean display = true;
     String labelString;
